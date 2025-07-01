878b1ffea37fb5e6692cdf84d768d1c2242cde09
dsa <dragons11@hotmail.com>
2025-07-01 15:10:13 +0300

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,14 +1,23 @@
 export default function handler(req, res) {
-    let status = process.env.STATUS || 1;
-    const time = new Date();
-    const hour = (time.getUTCHours() + 3) % 24;
+    res.setHeader('Access-Control-Allow-Origin', 'https://it-dsa.vercel.app');
+    try {
+        let status = parseInt(process.env.STATUS) || 1;
+        const time = new Date();
+        const hour = (time.getUTCHours() + 3) % 24;
 
-    if (hour >= 0 && hour < 10){
-        status = 4;
+        if (hour >= 0 && hour < 10){
+            status = 4;
+        }
+
+        res.status(200).json({
+            success: true,
+            status: status
+        })
+    } catch(error){
+        console.error("Error in status.js: ", error);
+        res.status(500).json({
+            success: false,
+            error: 'Internal server error'
+        });
     }
-
-    res.status(200).json({
-        success: true,
-        status: Number(status)
-    })
 }
