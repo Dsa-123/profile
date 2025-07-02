99988b8d3db6c48bd4e4c15e5c5485c4eac5e9f5
dsa <dragons11@hotmail.com>
2025-07-02 15:54:58 +0300

    Reset Var

--- api/visits.js
+++ api/visits.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   }
 
   else if(req.method === 'POST'){
-    await redis.set('visits', 70);
+    await redis.set('visits', 80);
     await redis.incr('visits');
 
     return res.status(200).json({
